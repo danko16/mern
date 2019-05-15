222eb04fb0959fc612b1de79325a1ce4eba5d6b4
danko16 <danangekoyudanto1995@gmail.com>
2019-05-15 15:35:00 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://indopro:indopro@cluster0-hsu1p.mongodb.net/test?retryWrites=true"
+  mongoURI: "mongodb+srv://indopro:indopro@cluster0-hsu1p.mongodb.net/MERN"
 }
