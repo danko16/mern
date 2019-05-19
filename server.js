77b0c448ea6b6ed53bdf4fba7f23e06bc1dd5cff
danang <danangekoyudanto1995@gmail.com>
2019-05-19 12:41:07 +0700

    danang

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const config = require('./config/config');
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
