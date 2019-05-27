d5981ecb344dcece02adf74aef1853008dc7bc79
danang <danangekoyudanto1995@gmail.com>
2019-05-27 15:30:05 +0700

    change to local mongodb

--- server.js
+++ server.js
@@ -24,13 +24,6 @@ app.use('/api/project', require('./routes/api/project'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(port, () => {
   console.log(`server now listening at port ${port}`);
 });
